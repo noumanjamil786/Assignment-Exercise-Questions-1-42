77f7ffef5f86933c0239f437b4902da68f869908
Nouman Jamil <159713811+noumanjamil786@users.noreply.github.com>
2024-05-24 18:54:12 +0500

    Initial commit

---
+++ 42.Great Magicians/main.ts
@@ -0,0 +1,24 @@
+// Great Magicians: Start with a copy of your program from Exercise 39. 
+// Write a function called make_great() that modifies the array of magicians by adding 
+// the phrase the Great to each magician’s name. Call show_magicians() to see that 
+// the list has actually been modified.
+
+let magicians1 : string[] = ["David Copperfield","Harry Blackstone Jr.","Siegfried "]
+
+function show_magicians2(magicians1 : any) {
+    for (let magician of magicians1) {
+      console.log(magician);
+    }
+  }
+
+  function make_great1(magicians1 : any) {
+    for (let i = 0; i < magicians1.length; i++) {
+      magicians1[i] += " the Great";
+    }
+  }
+  
+  show_magicians2(magicians1);
+  
+  make_great1(magicians1);
+  
+  show_magicians2(magicians1);
